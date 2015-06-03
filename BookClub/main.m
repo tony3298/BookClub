2a220b9a8c325b3d6fbb3c40dcf11fd3a1f75e78
tony3298 <tonydakhoul@yahoo.com>
2015-06-03 14:37:34 -0500

    Initial Commit

---
+++ BookClub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BookClub
+//
+//  Created by Tony Dakhoul on 6/3/15.
+//  Copyright (c) 2015 Tony Dakhoul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
